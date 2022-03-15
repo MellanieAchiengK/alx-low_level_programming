c80b9a77ba080b424882e79c5aa2dbef1abacbbe
Mellanie Achieng <mellanie.achieng@gmail.com>
2022-03-15 12:28:16 -0700

    betty style fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Return: 0
  */
@@ -15,5 +15,5 @@ void print_alphabet(void)
 		c++;
 	}
 	_putchar('\n');
-	
+
 }
