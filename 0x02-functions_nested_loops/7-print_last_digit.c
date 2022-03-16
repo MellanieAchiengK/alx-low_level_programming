d29278739ba9f799cd229adcf2beade2d24ce9f0
Mellanie Achieng <mellanie.achieng@gmail.com>
2022-03-16 06:02:04 -0700

    7-print_last_digit added

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *
+ * @n: integer
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		_putchar(n + '0');
+	}
+	return (n);
+}
