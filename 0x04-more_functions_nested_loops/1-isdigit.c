07aa5599d11f2d6f5926def31ca32a04b88ac87f
Mellanie Achieng <mellanie.achieng@gmail.com>
2022-03-17 05:45:56 -0700

    betty style fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,8 +2,8 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9)
- * @c - input
- * Return: 1 on success 0 otherwise 
+ * @c - digit input
+ * Return: 1 on success 0 otherwise
  */
 int _isdigit(int c)
 {
