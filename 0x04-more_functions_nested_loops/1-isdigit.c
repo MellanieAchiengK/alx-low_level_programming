94e4b929ba6d36f7d6eacbdb13137a70d855a5e1
Mellanie Achieng <mellanie.achieng@gmail.com>
2022-03-17 05:37:24 -0700

    1-isdigit added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c - input
+ * Return: 1 on success 0 otherwise 
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
