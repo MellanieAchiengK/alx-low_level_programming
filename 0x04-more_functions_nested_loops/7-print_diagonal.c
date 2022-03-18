3dc85fa2da7617cc5f6ae3c0bff5647dd809fcb0
mellanieachieng <mellanie.achieng@gmail.com>
2022-03-18 10:19:17 +0000

    7-print diagonal fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,19 @@
 
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i;
+	int j;
 
-	while (i < n && n > 0)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\\');
-		i++;
-	}
+		for (j = 0; j < n; j++)
+		{
+			if (j == i)
+				_putchar('\\');
+			else
+				_putchar(' ');
+		}
 
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
