73b88e9c48b0b1b17d0fe1febcd69156a00ebe88
mellanieachieng <mellanie.achieng@gmail.com>
2022-03-18 13:42:09 +0000

    8-print_square output fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,11 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
-			if (j == i)
-				_putchar('\\');
-			else
-				_putchar(' ');
+			_putchar('\\');
 		}
 
 		_putchar('\n');
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,24 @@
 #include "main.h"
 
 /**
- * print_square - prints a square
- *
- * @size: size of square
+ * print_square - draws a diagonal line
+ * @size: number of time # should be printed
  *
  * Return: 0
  */
 
 void print_square(int size)
 {
-	int i = 0;
+	int i;
+	int j;
 
-	while (i < size && size > 0)
+	for (i = 0; i < size; i++)
 	{
-		int j = 0;
-		while (j < size)
+		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
-			i++;
 		}
 
 		_putchar('\n');
-		i++;
 	}
-	if (i == 0)
-		_putchar('\n');
 }
