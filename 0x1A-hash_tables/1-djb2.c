47503fa0d8f43a1c8f596370a8e240d8d03888ae
mellanieachieng <mellanie.achieng@gmail.com>
2022-07-02 17:13:33 +0000

    betty style & documentaton fix

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,17 +3,17 @@
 /**
  * hash_djb2 - hash function using djb2 algorithm
  * @str: string to create hash from
- * Returnn: hash
+ * Return: hash
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
