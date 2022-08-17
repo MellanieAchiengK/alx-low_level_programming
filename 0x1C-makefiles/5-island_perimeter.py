cae3a4642f036edbdb054caca732bc54095ef4d1
mellanieachieng <mellanie.achieng@gmail.com>
2022-08-17 13:15:11 +0000

    Bug fix & advanced task added

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
-"""
-island perimeter
+""" island perimeter
 """
 
 
 def island_perimeter(grid):
-    """ returns perimeter of grid """
-    width = len(grid[0])
-    height = len(grid)
-    perimeter = 0
+    """ returns the perimeter of land
 
-    for c in range(height):
-        for h in range(width):
-            if grid[c][h] == 0:
-                if (h > 0 and grid[c][h - 1] == 1):
+    Args:
+        grid ([list of list]): map of the island
+    """
+    perimeter = 0
+    if not grid:
+        return perimeter
+    if not grid[0]:
+        return perimeter
+    num_col = len(grid[0])
+    for row in grid:
+        for actual in range(len(row)):
+            prev_val = actual - 1
+            next_val = actual + 1
+            if (row[actual] == 1):
+                if (row[prev_val] == 0 and prev_val >= 0):
                     perimeter += 1
-                if (c > 0 and grid[c - 1][h] == 1):
+                elif (prev_val < 0):
                     perimeter += 1
-                if (h < width - 1 and grid[c][h + 1] == 1):
+                if (next_val >= len(row)):
                     perimeter += 1
-                if (c < height - 1 and grid[c + 1][h] == 1):
+                elif (row[next_val] == 0):
                     perimeter += 1
-    return perimeter
+    for i in range(num_col):
+        column = [row[i] for row in grid]
+        for actual in range(len(column)):
+            prev_val = actual - 1
+            next_val = actual + 1
+            if (column[actual] == 1):
+                if (column[prev_val] == 0 and prev_val >= 0):
+                    perimeter += 1
+                elif (prev_val < 0):
+                    perimeter += 1
+                if next_val >= len(column):
+                    perimeter += 1
+                elif (column[next_val] == 0):
+                    perimeter += 1
+    return(perimeter)
